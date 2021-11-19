f6a371095507411a27598af6de7af9dcac1f0ac7
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-19 13:16:56 +0000

    Mongoose Models

--- schema/schema.js
+++ schema/schema.js
@@ -9,21 +9,24 @@ const {
   GraphQLList,
 } = graphql;
 
-// dummy data
-var books = [
-  { name: 'John Dies at the End', genre: 'Fantasy', id: '1', authorId: '1' },
-  { name: 'The Book With No Name', genre: 'Horror', id: '2', authorId: '2' },
-  { name: 'Solitaire', genre: 'Young Adult', id: '3', authorId: '3' },
-  { name: 'The Eye of the Moon', genre: 'Horror', id: '4', authorId: '2' },
-  { name: 'This Winter', genre: 'Young Adult', id: '5', authorId: '3' },
-  { name: 'Nick and Charlie', genre: 'Young Adult', id: '6', authorId: '3' },
-];
+const Book = require('../models/book');
+const Author = require('../models/author');
 
-var authors = [
-  { name: 'David Wong (Jason Pargin)', age: 46, id: '1' },
-  { name: 'Anonymous (The Bourbon Kid)', age: 0, id: '2' },
-  { name: 'Alice Oseman', age: 27, id: '3' },
-];
+// dummy data
+// var books = [
+//   { name: 'John Dies at the End', genre: 'Fantasy', id: '1', authorId: '1' },
+//   { name: 'The Book With No Name', genre: 'Horror', id: '2', authorId: '2' },
+//   { name: 'Solitaire', genre: 'Young Adult', id: '3', authorId: '3' },
+//   { name: 'The Eye of the Moon', genre: 'Horror', id: '4', authorId: '2' },
+//   { name: 'This Winter', genre: 'Young Adult', id: '5', authorId: '3' },
+//   { name: 'Nick and Charlie', genre: 'Young Adult', id: '6', authorId: '3' },
+// ];
+
+// var authors = [
+//   { name: 'David Wong (Jason Pargin)', age: 46, id: '1' },
+//   { name: 'Anonymous (The Bourbon Kid)', age: 0, id: '2' },
+//   { name: 'Alice Oseman', age: 27, id: '3' },
+// ];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -34,7 +37,7 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        return _.find(authors, { id: parent.authorId });
+        // return _.find(authors, { id: parent.authorId });
       },
     },
   }),
@@ -49,7 +52,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return _.filter(books, { authorId: parent.id });
+        // return _.filter(books, { authorId: parent.id });
       },
     },
   }),
@@ -62,26 +65,26 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(books, { id: args.id });
+        // return _.find(books, { id: args.id });
       },
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(authors, { id: args.id });
+        // return _.find(authors, { id: args.id });
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return books;
+        // return books;
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        return authors;
+        // return authors;
       },
     },
   },
