0291e3ddb8fc1677f05e3bb977315fca7c6a22b1
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-19 12:36:17 +0000

    Connecting to mLab

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
+require('dotenv').config();
 const app = express();
+
+mongoose.connect(
+  `mongodb+srv://admin:${process.env.DB_PASSWORD}@graphql.u5bs9.mongodb.net/graphqldata?retryWrites=true&w=majority`
+);
+mongoose.connection.once('open', () => {
+  console.log('Connected to Database');
+});
 
 app.use(
   '/graphql',
